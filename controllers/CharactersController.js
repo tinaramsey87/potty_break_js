5180dedb0dd6b4b86a7e9ea50b5962fb1aa55903
David Zulaica and Tina Ramsey <tina.ramsey87@gmail.com>
2015-02-26 10:29:58 -0800

    Form validator works (mostly—woohoo).

--- controllers/CharactersController.js
+++ controllers/CharactersController.js
@@ -1,11 +1,13 @@
 pottyBreak.controller('CharactersCtrl', function CharactersCtrl($scope, $state, CharactersFactory) {
   $scope.character = CharactersFactory.character;
   $scope.CharactersFactory = CharactersFactory;
-  $scope.createCharacter = function() {
+  $scope.createCharacter = function(isValid) {
+    if (isValid) {
     $scope.character.name = $scope.name;
     $scope.character.age = $scope.age;
     $scope.character.gender = $scope.gender;
     $state.go('lobby');
+    }
   }
-  if ($scope.name && $scope.age) { filled };
+
 });
